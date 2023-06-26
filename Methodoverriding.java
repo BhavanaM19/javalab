a943270774374ca51827af7a4bbdd23d5259d423
BhavanaM19 <76389447+BhavanaM19@users.noreply.github.com>
2023-06-26 14:46:40 +0530

    Add files via upload

---
+++ Methodoverriding.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+class Shape {
+    public double calculateArea() {
+        return 0.0;
+    }
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+}
+
+public class Methodoverriding {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(length, width);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+        Circle circle = new Circle(radius);
+
+        System.out.print("Enter the  side length of the square: ");
+        double side = scanner.nextDouble();
+        Square square = new Square(side);
+
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+        System.out.println("Area of Circle: " + circle.calculateArea());
+        System.out.println("Area of Square: " + square.calculateArea());
+    }
+}
+
